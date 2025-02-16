0a3fd59fc5b8a064d3fc960d6c02a9271dca3e14
MLDev33 <ypmarielaure@yahoo.fr>
2025-02-16 13:59:11 +0100

    S2 : MVC, test et code de la première interface graphique

---
+++ app/src/main/java/com/example/coach/modele/Profil.java
@@ -0,0 +1,91 @@
+package com.example.coach.modele;
+
+/**
+ * Classe qui permet de créer un Profil utilisateur dans le but de calculer son img
+ * (Indide Masse Grasse) en valorisant les paramètres necessaires au calcul.
+ */
+public class Profil {
+
+    // Constantes
+    private static final Integer minFemme = 15; // maigre si en dessous
+    private static final Integer maxFemme = 30; // gros si au dessus
+    private static final Integer minHomme = 10; // maigre si en dessous
+    private static final Integer maxHomme = 25; // gros si au dessus
+
+    private Integer poids;
+    private Integer taille;
+    private Integer age;
+    private Integer sexe;
+    private float img = 0;
+    private String message = "";
+
+    /**
+     * Constructeur : valorise directement les proriétés poids, taille, age, sexe
+     * @param poids
+     * @param taille
+     * @param age
+     * @param sexe 1 pour homme, 0 pour femme
+     */
+    public Profil(Integer poids, Integer taille, Integer age, Integer sexe) {
+        this.poids = poids;
+        this.taille = taille;
+        this.age = age;
+        this.sexe = sexe;
+    }
+
+    public Integer getPoids() {
+        return poids;
+    }
+
+    public Integer getTaille() {
+        return taille;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Integer getSexe() {
+        return sexe;
+    }
+
+    /**
+     * Retourne img après l'avoir calculé s'il est vide
+     * @return img
+     */
+    public float getImg() {
+        if(this.img == 0){
+            float tailleFloat = (float)this.getTaille() / 100 ;
+            img = (float)((1.2 * this.getPoids() / Math.pow(tailleFloat, 2) ) + (0.23 * this.getAge()) - (10.83 * this.getSexe()) - 5.4) ;
+        }
+        return img;
+    }
+
+    /**
+     * retourne le message correspondant à l'img
+     * @return message "normal", "trop faible", "trop élevé"
+     */
+    public String getMessage() {
+        img = getImg();
+        if(this.getSexe() == 0){
+            if(img < 15){
+                this.message = "trop faible";
+            } else if (img >= 15 && img <= 30) {
+                this.message = "normal";
+            } else {
+                this.message = "trop élevé";
+            }
+        } else if(this.getSexe() == 1){
+            if(img < 10){
+                this.message = "trop faible";
+            } else if (img >= 10 && img <= 25) {
+                this.message = "normal";
+            } else {
+                this.message = "trop élevé";
+            }
+        }
+        return message;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/coach/vue/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.coach.vue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.coach.R;
+import com.example.coach.controleur.Controle;
+
+/**
+ * Classe d'affichage (activity) MainActivity
+ * Permet le calcul d'un IMG
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtPoids;
+    private EditText txtTaille;
+    private EditText txtAge;
+    private RadioButton rdHomme;
+    private TextView lblIMG;
+    private ImageView imgSmiley;
+    private Button btnCalc;
+    private Controle controle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.init();
+    }
+
+    /**
+     * Initialisations à l'ouverture :
+     * - récupération des objets graphiques
+     * - création du contrôleur
+     * - demande d'écoute (évenements sur objets graphiques)
+     */
+    private void init(){
+        txtPoids = (EditText)findViewById(R.id.txtPoids);
+        txtTaille = (EditText)findViewById(R.id.txtTaille);
+        txtAge = (EditText)findViewById(R.id.txtAge);
+        rdHomme = (RadioButton)findViewById(R.id.rdHomme);
+        lblIMG= (TextView)findViewById(R.id.lblIMG);
+        imgSmiley = (ImageView)findViewById(R.id.imgSmiley);
+        btnCalc = (Button)findViewById(R.id.btnCalc);
+        controle = Controle.getInstance();
+        ecouteCalcul();
+    }
+
+    /**
+     * Ecoute l'événement clic sur le bouton btnCalc
+     */
+    private void ecouteCalcul(){
+        btnCalc.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Integer poids = 0;
+                Integer taille = 0;
+                Integer age = 0;
+                Integer sexe = 0;
+
+                try{
+                    poids = Integer.parseInt(txtPoids.getText().toString());
+                    taille = Integer.parseInt(txtTaille.getText().toString());
+                    age = Integer.parseInt(txtAge.getText().toString());
+                }catch(Exception e){}
+
+                if(rdHomme.isChecked()){
+                    sexe = 1;
+                }
+
+                if(poids == 0 || taille == 0 || age == 0){
+                    Toast.makeText(MainActivity.this, "Veuillez saisir tous les champs", Toast.LENGTH_SHORT).show();
+                }else{
+                    afficheResult(poids, taille, age, sexe);
+                }
+            }
+        });
+    }
+
+    /**
+     * Affiche l'image et le message correspondant au calcul de l'img
+     * @param poids
+     * @param taille
+     * @param age
+     * @param sexe
+     */
+    public void afficheResult(Integer poids, Integer taille, Integer age, Integer sexe) {
+        controle.creerProfil(poids, taille, age, sexe);
+        String message = controle.getMessage();
+        float img = controle.getImg();
+
+        if (message == "trop faible") {
+            imgSmiley.setImageResource(R.drawable.maigre);
+        } else if (message == "normal") {
+            imgSmiley.setImageResource(R.drawable.normal);
+        } else if (message == "trop élevé") {
+            imgSmiley.setImageResource(R.drawable.graisse);
+        }
+
+        String imgToSting = String.format("%.01f", img);
+
+        if (message == "normal") {
+            lblIMG.setTextColor(Color.GREEN);
+        } else {
+            lblIMG.setTextColor(Color.RED);
+        }
+
+        lblIMG.setText(imgToSting + " :IMG " + message);
+    }
+
+}
